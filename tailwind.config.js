cc08e1f59e08840f1734697655bad6c6da56bdab
Yuniarrr <5027211025@mhs.its.ac.id>
2023-11-22 16:59:44 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      pastelPink: "#2e1065",
+      red: "#b91c1c",
+    },
+  },
+  plugins: [],
+};
