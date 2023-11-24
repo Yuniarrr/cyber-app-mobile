64201ffcd090bf43283109c0135fae382746f550
Yuniarrr <5027211025@mhs.its.ac.id>
2023-11-24 09:48:44 +0700

    fix: background

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 
 export default function Background({ children }) {
   return (
-    <View className="flex-1 items-center justify-center bg-[#FFFFFF]">
+    <SafeAreaView className="flex-1 bg-[#FFFFFF]">
       {children}
-    </View>
+    </SafeAreaView>
   );
 }
